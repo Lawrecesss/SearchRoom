875ba2145522dabfd52bef588aacd9d235908804
Lawrence <lhshein14@gmail.com>
2024-03-15 21:53:25 +0800

    test api

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import userRouter from "./routers/user.router.js";
 dotenv.config();
 
 mongoose
@@ -16,3 +17,4 @@ const app = express();
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
+app.use("/api/user", userRouter);
